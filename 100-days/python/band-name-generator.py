2cf24fa004189470204fb833bba4b044ee11e144
sweta pandey <swetapandey4190@gmail.com>
2025-04-30 17:50:50 +0530

    Descriptive commit message

---
+++ 100-days/python/band-name-generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name generator")
+city = input("enter your city name \n")
+pet = input("enter your pet name \n")
+brand = city + " "+ pet
+print("your band name is : " ,brand)
